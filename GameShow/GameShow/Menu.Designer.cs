54b604f11aab45424979ad74bb04d29fb7e1aed6
phelinor <gera.glez@gmail.com>
2019-04-02 16:04:49 -0600

    Menu Screen

---
+++ GameShow/GameShow/Menu.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GameShow
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTeams = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTeams
+            // 
+            this.btnTeams.BackColor = System.Drawing.Color.Transparent;
+            this.btnTeams.FlatAppearance.BorderSize = 0;
+            this.btnTeams.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeams.ForeColor = System.Drawing.Color.White;
+            this.btnTeams.Location = new System.Drawing.Point(223, 194);
+            this.btnTeams.Name = "btnTeams";
+            this.btnTeams.Size = new System.Drawing.Size(320, 69);
+            this.btnTeams.TabIndex = 0;
+            this.btnTeams.Text = "Enter Team Names";
+            this.btnTeams.UseVisualStyleBackColor = false;
+            // 
+            // btnTest
+            // 
+            this.btnTest.BackColor = System.Drawing.Color.Transparent;
+            this.btnTest.FlatAppearance.BorderSize = 0;
+            this.btnTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTest.ForeColor = System.Drawing.Color.White;
+            this.btnTest.Location = new System.Drawing.Point(223, 304);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(320, 69);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test Buzzers";
+            this.btnTest.UseVisualStyleBackColor = false;
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.BackColor = System.Drawing.Color.Transparent;
+            this.btnStartGame.FlatAppearance.BorderSize = 0;
+            this.btnStartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartGame.ForeColor = System.Drawing.Color.White;
+            this.btnStartGame.Location = new System.Drawing.Point(223, 415);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(320, 69);
+            this.btnStartGame.TabIndex = 2;
+            this.btnStartGame.Text = "Begin Game";
+            this.btnStartGame.UseVisualStyleBackColor = false;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GameShow.Properties.Resources.OneNumberLogo1;
+            this.pictureBox1.InitialImage = global::GameShow.Properties.Resources.OneNumberLogo1;
+            this.pictureBox1.Location = new System.Drawing.Point(223, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 92);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(167)))), ((int)(((byte)(219)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnTeams);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTeams;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ GameShow/GameShow/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameShow
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            ReadyScreen rs = new ReadyScreen();
+            rs.Show();
+        }
+    }
+}
