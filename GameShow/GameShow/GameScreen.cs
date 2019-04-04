bd37e331d1fe09546a800b67b5a88bba8d238de3
phelinor <gera.glez@gmail.com>
2019-04-04 15:46:38 -0600

    Teams Text File

--- GameShow/GameShow/GameScreen.cs
+++ GameShow/GameShow/GameScreen.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GameShow
 {
@@ -15,12 +16,26 @@ namespace GameShow
         private System.Windows.Forms.Label[] lblTeams;
         private void loadTeams()
         {
-            // 
-            // lblTeams
-            //
-            this.lblTeams = new System.Windows.Forms.Label[10];
-            for (int i=0;i<10;i++)
+            String[] teams = null;
+            try
             {
+                //this file stores the list of teams
+                String teamsFile = File.ReadAllText("teams.txt");
+                teams = teamsFile.Split('\n');
+            }
+            catch (Exception e)
+            {
+                teams = new String[4];
+                teams[0] = "John";
+                teams[1] = "Mary";
+                teams[2] = "Carol";
+                teams[3] = "Ken";
+            }
+            // Here we dinamically create Label for each team on the 
+            this.lblTeams = new System.Windows.Forms.Label[teams.Length];
+            for (int i = 0; i < teams.Length; i++)
+            {
+                teams[i] = teams[i].Replace("\r", ""); //Need to remove new line char at end of line. Optionally use System.Environment.NewLine
                 this.lblTeams[i] = new System.Windows.Forms.Label();
                 this.lblTeams[i].BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
                 this.lblTeams[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -31,7 +46,7 @@ namespace GameShow
                 this.lblTeams[i].Name = "lblTeams" + i;
                 this.lblTeams[i].Size = new System.Drawing.Size(186, 46);
                 this.lblTeams[i].TabIndex = 15;
-                this.lblTeams[i].Text = "John";
+                this.lblTeams[i].Text = teams[i];
                 this.lblTeams[i].TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
                 this.Controls.Add(this.lblTeams[i]);
             }
